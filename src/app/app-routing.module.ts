80f612c25831638e6e3421bc7f284ec5f8b28def
Mikl Aleinikov <sirmikl.aleinikov@gmail.com>
2021-02-25 21:49:13 +0300

    added all components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {RouterModule, Routes} from '@angular/router';
-import {LoginComponent} from './components/login/login.component';
-import {FacebookGuard} from './guards/facebook.guard';
-import {HomeComponent} from './components/home/home.component';
-import {FormsModule} from '@angular/forms';
-import {MatCardModule} from '@angular/material/card';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-    canActivate: [FacebookGuard],
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-  }];
 
 @NgModule({
-  declarations: [HomeComponent, LoginComponent],
+  declarations: [],
   imports: [
-    CommonModule,
-    RouterModule.forRoot(routes),
-    FormsModule,
-    MatCardModule
-  ],
-  exports: [
-    RouterModule
+    CommonModule
   ]
 })
 export class AppRoutingModule { }
